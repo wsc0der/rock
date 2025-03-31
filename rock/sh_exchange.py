96a1039bfa4158fc0807e8a39e8945a12665203c
wsc0der <wsc0der@outlook.com>
2025-03-31 04:20:33 +0000

    Introducing mypy and refactoring code

--- rock/sh_exchange.py
+++ rock/sh_exchange.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import requests
 import pandas as pd
 
-def get_delistings():
+def get_delistings() -> pd.DataFrame:
     """
     Fetch delisting data from the Shanghai Stock Exchange (SSE).
 
@@ -13,14 +13,34 @@ def get_delistings():
     by making an HTTP GET request to the specified endpoint. The response is expected
     to be in Excel format, which is then parsed into a Pandas DataFrame.
 
-    Endpoint: https://www.sse.com.cn/assortment/stock/list/delisting/
+    https://www.sse.com.cn/assortment/stock/list/delisting/
 
     Returns:
         pd.DataFrame: A DataFrame containing delisting data.
 
     Raises:
         ValueError: If the response's Content-Type is not expected".
-        requests.exceptions.RequestException: If the HTTP request fails (e.g., timeout, connection error).
+        requests.exceptions.RequestException:
+            If the HTTP request fails (e.g., timeout, connection error).
+    """
+
+    response = sse_query((1,2), 3)
+    response.raise_for_status()
+
+    # Check Content-Type
+    expect_type = "application/vnd.ms-excel"
+    content_type = response.headers.get("Content-Type")
+    if content_type and expect_type not in content_type:
+        raise ValueError(f"Unexpected Content-Type: {content_type}")
+
+    return pd.read_excel(BytesIO(response.content))
+
+
+def sse_query(stock_type: tuple[int, ...] = (1,), company_status: int = 1) -> requests.Response:
+    """
+    Fetch data from the Shanghai Stock Exchange (SSE) using a specific SQL query.
+    This function constructs a URL with the provided parameters and makes an HTTP GET
+    request to the SSE's official website. The function returns the raw response object.
     """
     headers = {
         "Accept": "*/*",
@@ -31,17 +51,16 @@ def get_delistings():
         "Host": "query.sse.com.cn",
         "Pragma": "no-cache",
         "Referer": "https://www.sse.com.cn/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/97.0.4692.71 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
     }
-    url ='https://query.sse.com.cn//sseQuery/commonExcelDd.do?sqlId=COMMON_SSE_CP_GPJCTPZ_GPLB_ZZGP_L&type=inParams&STOCK_CODE=&REG_PROVINCE=&STOCK_TYPE=1,2&COMPANY_STATUS=3'
-    response = requests.get(url, headers=headers, timeout=10)
-    response.raise_for_status()
+    stock_type_str = ','.join(map(str, stock_type))
+    url = (f'https://query.sse.com.cn//sseQuery/commonExcelDd.do?'
+           f'sqlId=COMMON_SSE_CP_GPJCTPZ_GPLB_ZZGP_L'
+           f'&type=inParams&STOCK_CODE=&REG_PROVINCE='
+           f'&STOCK_TYPE={stock_type_str}'
+           f'&COMPANY_STATUS={company_status}'
+    )
 
-    # Check Content-Type
-    expect_type = "application/vnd.ms-excel"
-    content_type = response.headers.get("Content-Type")
-    if expect_type not in content_type:
-        raise ValueError(f"Unexpected Content-Type: {content_type}")
-
-    return pd.read_excel(BytesIO(response.content))
+    # Make the request
+    return requests.get(url, headers=headers, timeout=10)
--- tests/test_sh_exchange.py
+++ tests/test_sh_exchange.py
@@ -5,18 +5,24 @@ Test cases for sh_exchange module.
 import unittest
 from pandas import DataFrame
 from requests.exceptions import HTTPError
-from rock import sh_exchange as ex
+from requests import Response
+from rock import sh_exchange
 
 class TestShExchange(unittest.TestCase):
     """Test cases for sh_exchange module"""
 
-    def test_get_delistings(self):
+    def test_get_delistings(self) -> None:
         """Test function."""
         try:
-            delistings = ex.get_delistings()
+            delistings = sh_exchange.get_delistings()
             self.assertTrue(isinstance(delistings, DataFrame), "Delistings should be a DataFrame")
             self.assertGreater(len(delistings), 0, "Delistings should not be empty")
         except ValueError as e:
             self.fail(f"ValueError raised: {e}")
         except HTTPError as e:
             self.fail(f"HTTPError raised: {e}")
+
+    def test_sse_query(self) -> None:
+        """Test function."""
+        response = sh_exchange.sse_query((1,), 3)
+        self.assertTrue(isinstance(response, Response), "Response should be a Response object")
