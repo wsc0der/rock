1bdeababd0c20e0be289a8933a52fbb35136ad89
wsc0der <wsc0der@outlook.com>
2025-04-01 04:25:17 +0000

    Status and type filter are supported when fetching stock list.

--- rock/sh_exchange.py
+++ rock/sh_exchange.py
@@ -1,46 +1,129 @@
 """
+
 This modules provides functions for getting data from SSE.
 """
 from io import BytesIO
+from enum import IntEnum, StrEnum
+from typing import Type, TypeVar
 import requests
 import pandas as pd
 
-def get_delistings() -> pd.DataFrame:
+
+class StockType(IntEnum):
     """
-    Fetch delisting data from the Shanghai Stock Exchange (SSE).
+    Enum for stock types on the Shanghai Stock Exchange (SSE).
+    """
+    A = 1
+    B = 2
+    KECHUANG = 8
 
-    This function retrieves delisting information from the SSE's official website
-    by making an HTTP GET request to the specified endpoint. The response is expected
-    to be in Excel format, which is then parsed into a Pandas DataFrame.
 
-    https://www.sse.com.cn/assortment/stock/list/delisting/
+class StockStatus(IntEnum):
+    """
+    Enum for company status on the Shanghai Stock Exchange (SSE).
+    """
+    NORMAL = 2
+    DELISTED = 3
+    ST = 7
 
+
+class ContentType(StrEnum):
+    """
+    Enum for raw data types returned by querying SSE endpoints.
+    """
+    EXCEL = "application/vnd.ms-excel"
+
+
+def get_stock_list(types_in: str|list[StockType], status_in: str|list[StockStatus]) -> pd.DataFrame:
+    """
+    Get stock list from the Shanghai Stock Exchange (SSE) based on stock types and statuses.
+    This function fetches stock data from the SSE using Http request and returns
+    the data as a Pandas DataFrame. The function supports filtering by stock types and
+    statuses using the provided parameters.
+    Resources: https://www.sse.com.cn/assortment/stock/list/share/
+    Args:
+        types_in (str|list[StockType]): A string or list of stock types to filter the query.
+        status_in (str|list[StockStatus]): A string or list of stock statuses to filter the query.
     Returns:
-        pd.DataFrame: A DataFrame containing delisting data.
-
+        pd.DataFrame: A DataFrame containing the stock data.
     Raises:
-        ValueError: If the response's Content-Type is not expected".
-        requests.exceptions.RequestException:
-            If the HTTP request fails (e.g., timeout, connection error).
+        TypeError: If types_in or status_in are not of the expected types.
+        ValueError: If the input values are invalid or if the response content is not as expected.
     """
 
-    response = sse_query((1,2), 3)
+    T = TypeVar('T', bound='IntEnum')
+    def process_input(ipt: str|list[T], cls: Type[T]) -> list[int]:
+        try:
+            if isinstance(ipt, str):
+                return list(map(cls, (map(int, ipt.replace(',', ' ').split()))))
+            return list(map(cls, ipt))
+        except ValueError as e:
+            raise ValueError(
+                f"Invalid input: {ipt}. Expected a string or list of {cls.__name__} values.") from e
+
+    if not isinstance(types_in, (str, list)):
+        raise TypeError("types_in must be a string or a list of StockType")
+    if not isinstance(status_in, (str, list)):
+        raise TypeError("status_in must be a string or a list of StockStatus")
+
+    types = process_input(types_in, StockType)
+    status = process_input(status_in, StockStatus)
+
+    response = _sse_query(
+        ','.join(map(str, types)),
+        ','.join(map(str, status))
+    )
     response.raise_for_status()
 
+    return _get_data_by_type(response, ContentType.EXCEL)
+
+
+def _get_data_by_type(response: requests.Response, expected_type: ContentType) -> pd.DataFrame:
+    """
+    Parse the response content based on the expected type.
+    This function checks the Content-Type of the response and raises an error if it
+    does not match the expected type. If the Content-Type is correct, it parses the
+    content into a Pandas DataFrame.
+    Args:
+        response (requests.Response): The HTTP response object.
+        expected_type (ContentType): The expected Content-Type of the response.
+    Returns:
+        pd.DataFrame: A DataFrame containing the parsed data.
+    Raises:
+        ValueError: If content doesn't exist, the response's Content-Type does not
+            match the expected type or the expected_type is unsupported.
+    """
+
     # Check Content-Type
-    expect_type = "application/vnd.ms-excel"
     content_type = response.headers.get("Content-Type")
-    if content_type and expect_type not in content_type:
+    if content_type is None:
+        raise ValueError("Content-Type header is missing in the response")
+    if expected_type not in content_type:
         raise ValueError(f"Unexpected Content-Type: {content_type}")
 
-    return pd.read_excel(BytesIO(response.content))
+    # Parse the response content based on the expected type
+    result = None
+    match expected_type:
+        case ContentType.EXCEL:
+            result = pd.read_excel(BytesIO(response.content))
+        case _:
+            raise ValueError(f"Unsupported Content-Type: {expected_type}")
+
+    return result
 
 
-def sse_query(stock_type: tuple[int, ...] = (1,), company_status: int = 1) -> requests.Response:
+def _sse_query(types: str, status: str) -> requests.Response:
     """
     Fetch data from the Shanghai Stock Exchange (SSE) using a specific SQL query.
     This function constructs a URL with the provided parameters and makes an HTTP GET
     request to the SSE's official website. The function returns the raw response object.
+    Args:
+        types (str): A comma seperated int string representing the stock types to filter the query.
+        status (str): A comma seperated int string representing the stock statuses to filter the query.
+    Returns:
+        requests.Response: The raw response object from the HTTP GET request.
+    Raises:
+        HTTPError: If the request to the SSE fails.
     """
     headers = {
         "Accept": "*/*",
@@ -54,13 +137,15 @@ def sse_query(stock_type: tuple[int, ...] = (1,), company_status: int = 1) -> re
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
         "(KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
     }
-    stock_type_str = ','.join(map(str, stock_type))
+
     url = (f'https://query.sse.com.cn//sseQuery/commonExcelDd.do?'
            f'sqlId=COMMON_SSE_CP_GPJCTPZ_GPLB_ZZGP_L'
            f'&type=inParams&STOCK_CODE=&REG_PROVINCE='
-           f'&STOCK_TYPE={stock_type_str}'
-           f'&COMPANY_STATUS={company_status}'
+           f'&STOCK_TYPE={types}'
+           f'&COMPANY_STATUS={status}'
     )
 
     # Make the request
-    return requests.get(url, headers=headers, timeout=10)
+    response = requests.get(url, headers=headers, timeout=10)
+    response.raise_for_status()
+    return response
--- tests/test_sh_exchange.py
+++ tests/test_sh_exchange.py
@@ -4,25 +4,48 @@ Test cases for sh_exchange module.
 
 import unittest
 from pandas import DataFrame
-from requests.exceptions import HTTPError
-from requests import Response
-from rock import sh_exchange
+from rock import sh_exchange as exchange
 
 class TestShExchange(unittest.TestCase):
     """Test cases for sh_exchange module"""
 
-    def test_get_delistings(self) -> None:
+    def test_get_stock_list(self) -> None:
         """Test function."""
-        try:
-            delistings = sh_exchange.get_delistings()
-            self.assertTrue(isinstance(delistings, DataFrame), "Delistings should be a DataFrame")
-            self.assertGreater(len(delistings), 0, "Delistings should not be empty")
-        except ValueError as e:
-            self.fail(f"ValueError raised: {e}")
-        except HTTPError as e:
-            self.fail(f"HTTPError raised: {e}")
+        normal_cases = [
+            ('1', '2'),
+            ('1,2', '2,3'),
+            ('', ''),
+            ([], []),
+            ([1,2], [2]),
+            ([exchange.StockType.A], [exchange.StockStatus.NORMAL]),
+            ([exchange.StockType.A], [exchange.StockStatus.NORMAL, exchange.StockStatus.ST])
+        ]
 
-    def test_sse_query(self) -> None:
-        """Test function."""
-        response = sh_exchange.sse_query((1,), 3)
-        self.assertTrue(isinstance(response, Response), "Response should be a Response object")
+        for case in normal_cases:
+            with self.subTest():
+                stock_list = exchange.get_stock_list(*case) # type: ignore
+                self.assertTrue(
+                    isinstance(stock_list, DataFrame),
+                    "Stock list should be a DataFrame"
+                )
+
+        type_exceptions = [
+            ([exchange.StockType.A,], tuple()),
+            (tuple(), [exchange.StockStatus.DELISTED])
+        ]
+
+        for case in type_exceptions:
+            with self.subTest():
+                with self.assertRaises(TypeError):
+                    exchange.get_stock_list(*case)  # type: ignore
+
+        value_exceptions = [
+            (["1"], ["2"]),
+            (["1", 1], [2, "3"]),
+            ([8, 9], [2, -1])
+        ]
+
+        for case in value_exceptions:   # type: ignore
+            with self.subTest():
+                with self.assertRaises(ValueError):
+                    exchange.get_stock_list(*case)  # type: ignore
