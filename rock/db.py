10c0358472eefde1583aee536c82721771f6d192
wsc0der <wsc0der@outlook.com>
2025-04-10 05:47:34 +0000

    feat(db): Add more constratints to table columns

--- rock/db.py
+++ rock/db.py
@@ -16,121 +16,147 @@ class Tables(StrEnum):
     EXCHANGE = 'exchange'
     HISTORY = 'history'
 
-def init():
+def init() -> None:
     """Initialize the database."""
     if os.path.exists(DB_PATH):
-        print(f'Database {DB_NAME} already exists.')
+        print(f'Database {DB_NAME} is already exist.')
         return
 
     os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+    connection = get_db_connection()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS {Tables.SECURITY} (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                symbol TEXT NOT NULL UNIQUE CHECK (symbol != ''),
+                name TEXT NOT NULL UNIQUE CHECK (name != ''),
+                type TEXT CHECK (type IN ('stock', 'bond', 'fund')),
+                exchange_id INTEGER REFERENCES {Tables.EXCHANGE}(id)
+            )
+        ''')
+
+        cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS {Tables.EXCHANGE} (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE CHECK (name != ''),
+                acronym TEXT NOT NULL UNIQUE CHECK (acronym != ''),
+                type TEXT NOT NULL CHECK (type IN ('stock', 'bond', 'fund'))
+            )
+        ''')
+
+        cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS {Tables.HISTORY} (
+                security_id INTEGER NOT NULL REFERENCES {Tables.SECURITY}(id),
+                date TEXT NOT NULL ChECK (date != ''),
+                open REAL NOT NULL,
+                close REAL NOT NULL,
+                high REAL NOT NULL,
+                low REAL NOT NULL,
+                adj_close REAL NOT NULL,
+                volume INTEGER NOT NULL CHECK (volume >= 0),
+                frequency TEXT NOT NULL CHECK (frequency IN ('1m', '1d')),
+                PRIMARY KEY (security_id, date)
+            )
+        ''')
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
+
+
+def get_db_connection() -> sqlite3.Connection:
+    """Get a database connection."""
     connection = sqlite3.connect(DB_PATH)
-
-    cursor = connection.cursor()
-    cursor.execute(f'''
-        CREATE TABLE IF NOT EXISTS {Tables.SECURITY} (
-            id INTEGER PRIMARY KEY,
-            symbol TEXT NOT NULL,
-            name TEXT NOT NULL,
-            type TEXT CHECK (type IN ('stock', 'bond', 'fund')),
-            exchange_id INTEGER REFERENCES exchange(id)
-        )
-    ''')
-
-    cursor.execute(f'''
-        CREATE TABLE IF NOT EXISTS {Tables.EXCHANGE} (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            acronym TEXT NOT NULL,
-            type TEXT NOT NULL CHECK (type IN ('stock', 'bond', 'fund'))
-        )
-    ''')
-
-    cursor.execute(f'''
-        CREATE TABLE IF NOT EXISTS {Tables.HISTORY} (
-            id INTEGER PRIMARY KEY,
-            security_id INTEGER REFERENCES security(id),
-            date TEXT NOT NULL,
-            open REAL NOT NULL,
-            close REAL NOT NULL,
-            high REAL NOT NULL,
-            low REAL NOT NULL,
-            adj_close REAL NOT NULL,
-            volume INTEGER NOT NULL,
-            frequency TEXT CHECK (frequency IN ('1m', '1d'))
-        )
-    ''')
-
-    connection.commit()
-    connection.close()
+    connection.execute('PRAGMA foreign_keys = ON;')
+    return connection
 
 
-def insert_exchange(name: str, acronym: str, type_: str):
+def insert_exchange(name: str, acronym: str, exchange_type: str) -> None:
     """Insert exchange data into the database."""
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
-    cursor.execute(f'''
-        INSERT INTO {Tables.EXCHANGE} (name, acronym, type)
-        VALUES (?, ?, ?)
-    ''', (name, acronym, type_))
-    connection.commit()
-    connection.close()
+    connection = get_db_connection()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(f'''
+            INSERT INTO {Tables.EXCHANGE} (name, acronym, type)
+            VALUES (?, ?, ?)
+        ''', (name, acronym, exchange_type))
+        cursor.close()
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
 
 
-def insert_security(symbol: str, name: str, type_: str, exchange_id: int):
+def insert_security(symbol: str, name: str, symbol_type: str, exchange_id: int) -> None:
     """Insert security data into the database."""
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
-    cursor.execute(f'''
-        INSERT INTO {Tables.SECURITY} (symbol, name, type, exchange_id)
-        VALUES (?, ?, ?, ?)
-    ''', (symbol, name, type_, exchange_id))
-    connection.commit()
-    connection.close()
+    connection = get_db_connection()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(f'''
+            INSERT INTO {Tables.SECURITY} (symbol, name, type, exchange_id)
+            VALUES (?, ?, ?, ?)
+        ''', (symbol, name, symbol_type, exchange_id))
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
 
 
-def insert_securities(securities: list[tuple[str, str, str, int]]):
+def insert_securities(securities: list[tuple[str, str, str, int]]) -> None:
     """Insert multiple securities into the database."""
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
-    cursor.executemany(f'''
-        INSERT INTO {Tables.SECURITY} (symbol, name, type, exchange_id)
-        VALUES (?, ?, ?, ?)
-    ''', securities)
-    connection.commit()
-    connection.close()
+    connection = get_db_connection()
+    try:
+        cursor = connection.cursor()
+        cursor.executemany(f'''
+            INSERT INTO {Tables.SECURITY} (symbol, name, type, exchange_id)
+            VALUES (?, ?, ?, ?)
+        ''', securities)
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
 
 
-def insert_history(security_id: int, date: str, open_: float, close_: float,
-                  high_: float, low_: float, adj_close: float, volume: int, frequency: str):
+def insert_history(security_id: int, date: str, open_price: float, close_price: float,
+                  high_price: float, low_price: float, adj_close: float, volume: int, frequency: str) -> None:
     """Insert history data into the database."""
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
-    cursor.execute(f'''
-        INSERT INTO {Tables.HISTORY} (security_id, date, open, close, high, low, adj_close, volume, frequency)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', (security_id, date, open_, close_, high_, low_, adj_close, volume, frequency))
-    connection.commit()
-    connection.close()
+    connection = get_db_connection()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(f'''
+            INSERT INTO {Tables.HISTORY} (security_id, date, open, close, high, low, adj_close, volume, frequency)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (security_id, date, open_price, close_price, high_price, low_price, adj_close, volume, frequency))
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
 
 
-def insert_prices(prices: list[tuple[int, str, float, float, float, float, float, int, str]]):
+def insert_prices(prices: list[tuple[int, str, float, float, float, float, float, int, str]]) -> None:
     """Insert multiple prices into the database."""
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
-    cursor.executemany(f'''
-        INSERT INTO {Tables.HISTORY} (security_id, date, open, close, high, low, adj_close, volume, frequency)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', prices)
-    connection.commit()
-    connection.close()
+    connection = get_db_connection()
+    try:
+        cursor = connection.cursor()
+        cursor.executemany(f'''
+            INSERT INTO {Tables.HISTORY} (security_id, date, open, close, high, low, adj_close, volume, frequency)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', prices)
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
 
 
 def get_security(symbol: str) -> DataFrame:
     """Get security data from the database."""
-    connection = sqlite3.connect(DB_PATH)
-    query = f'''
-        SELECT * FROM {Tables.SECURITY} WHERE symbol=?
-    '''
-    df = read_sql(query, connection, params=(symbol,))
-    connection.close()
+    connection = get_db_connection()
+    try:
+        query = f'''
+            SELECT * FROM {Tables.SECURITY} WHERE symbol=?
+        '''
+        df = read_sql(query, connection, params=(symbol,))
+    finally:
+        connection.close()
     return df
--- tests/test_db.py
+++ tests/test_db.py
@@ -12,7 +12,7 @@ class TestDatabase(unittest.TestCase):
     """Test cases for the database module."""
     def setUp(self):
         db.init()
-        self.connection = sqlite3.connect(db.DB_PATH)
+        self.connection = db.get_db_connection()
         return super().setUp()
 
     def tearDown(self):
@@ -36,9 +36,28 @@ class TestDatabase(unittest.TestCase):
         cursor = self.connection.cursor()
         cursor.execute(f"SELECT * FROM {db.Tables.EXCHANGE} WHERE acronym='SSE';")
         exchange = cursor.fetchone()
+        cursor.close()
 
         self.assertIsNotNone(exchange, "Exchange should be inserted into the database.")
         self.assertEqual(exchange[1], 'Shanghai Stock Exchange', "Exchange name should match.")
+
+        # Test invalid cases
+        invalid_cases = [
+            (None, 'NNM', 'stock'),                     # name is None
+            ('', 'EPT', 'stock'),                       # name is empty
+            ('None Acronym Exchange', None, 'stock'),   # acronym is None
+            ('Empty Acronym Exchange', '', 'stock'),    # acronym is empty
+            ('Shanghai Stock Exchange', 'ABC', 'stock'),    # name is not unique
+            ('Another Stock Exchange',  'SSE', 'stock'),    # acronym is not unique
+            ('Fake Stock Exchange', 'FSE', 'invalid_type'), # type is invalid
+            ('Fake Stock Exchange', 'FSE', ''),             # type is empty
+            ('Fake Stock Exchange', 'FSE', None),           # type is None
+        ]
+
+        for case in invalid_cases:
+            with self.subTest():
+                with self.assertRaises(sqlite3.IntegrityError, msg=case):
+                    db.insert_exchange(*case)
 
     def test_insert_security(self):
         """Test inserting a security into the database."""
@@ -52,11 +71,29 @@ class TestDatabase(unittest.TestCase):
         cursor = self.connection.cursor()
         cursor.execute(f"SELECT * FROM {db.Tables.SECURITY} WHERE symbol='000001';")
         security = cursor.fetchone()
+        cursor.close()
 
         self.assertIsNotNone(security, "Security should be inserted into the database.")
         self.assertEqual(security[1], '000001', "Security symbol should match.")
         self.assertEqual(security[2], 'Ping An Bank', "Security name should match.")
         self.assertEqual(security[3], 'stock', "Security type should match.")
+
+        # Test invalid cases
+        invalid_cases = [
+            (None, 'None Company', 'stock', 1),     # symbol is None
+            ('', 'Empty Company', 'stock', 1),      # symbol is empty
+            ('000002', None, 'stock', 1),           # name is None
+            ('000003', '', 'stock', 1),             # name is empty
+            ('000001', 'Another Company', 'stock', 1),  # symbol is not unique
+            ('000002', 'Ping An Bank', 'stock', 1),     # name is not unique
+            ('000003', 'Invalid Company', 'invalid_type', 1),    # type is invalid
+            ('000004', 'Second Invalid Company', 'stock', 999)     # exchange_id is not valid
+        ]
+
+        for case in invalid_cases:
+            with self.subTest():
+                with self.assertRaises(sqlite3.IntegrityError, msg=case):
+                    db.insert_security(*case)
 
     def test_insert_price(self):
         """Test inserting a price into the database."""
@@ -73,9 +110,35 @@ class TestDatabase(unittest.TestCase):
         cursor = self.connection.cursor()
         cursor.execute(f"SELECT * FROM {db.Tables.HISTORY} WHERE date='2025-03-01';")
         price = cursor.fetchone()
+        cursor.close()
 
         self.assertIsNotNone(price, "Price should be inserted into the database.")
-        self.assertEqual(price[2], '2025-03-01', "Price date should match.")
+        self.assertEqual(price[1], '2025-03-01', "Price date should match.")
+
+        # Test invalid cases
+        invalid_cases = [
+            (None, '2025-03-01', 10.0, 11.0, 12.0, 9.0, 10.5, 1000, '1d'),  # security_id is None
+            (0, None, 10.0, 11.0, 12.0, 9.0, 10.5, 1000, '1d'),             # date is None
+            (1, '', 10.0, 11.0, 12.0, 9.0, 10.5, 1000, '1d'),               # date is empty
+            (0, '2025-03-01', 10.0, 11.0, 12.0, 9.0, 10.5, 1000, '1d'),     # security_id is invalid
+            (999, '2025-03-01', 10.0, 11.0, 12.0, 9.0, 10.5, 1000, '1d'),   # security_id is invalid
+            (1, '2025-03-01', 10.0, 11.0, 12.0, 9.0, 10.5, 1000, '1d'),     # primary key is duplicated
+            (1, '2025-03-02', None, 11.0, 12.0, 9.0, 10.5, 1000, '1d'),     # open_price is None
+            (1, '2025-03-02', 10.0, None, 12.0, 9.0, 10.5, 1000, '1d'),     # close_price is None
+            (1, '2025-03-02', 10.0, 11.0, None, 9.0, 10.5, 1000, '1d'),     # high_price is None
+            (1, '2025-03-02', 10.0, 11.0, 12.0, None, 10.5, 1000, '1d'),    # low_price is None
+            (1, '2025-03-02', 10.0, 11.0, 12.0, 9.0, None, 1000, '1d'),     # adj_close is None
+            (1, '2025-03-02', 10.0, 11.0, 12.0, 9.0, 10.5, None, '1d'),     # volume is None
+            (1, '2025-03-02', 10.0, 11.0, 12.0, 9.0, 10.5, -1000, '1d'),    # volume is negative
+            (1, '2025-03-02', 10.0, 11.0, 12.0, 9.0, 10.5, 1000, None),     # frequency is None
+            (1, '2025-03-02', 10.0, 11.0, 12.0, 9.0, 10.5, 1000, ''),       # frequency is empty
+            (1, '2025-03-02', 10.0, 11.0, 12.0, 9.0, 10.5, 1000, 'invalid') # frequency is invalid
+        ]
+
+        for case in invalid_cases:
+            with self.subTest():
+                with self.assertRaises(sqlite3.IntegrityError, msg=case):
+                    db.insert_history(*case)
 
     def test_insert_prices(self):
         """Test inserting multiple prices into the database."""
@@ -100,10 +163,29 @@ class TestDatabase(unittest.TestCase):
         cursor.execute(f"SELECT * FROM {db.Tables.HISTORY} WHERE date='2025-03-02';")
         price_2 = cursor.fetchone()
 
-        assert price_1 is not None and price_2 is not None, "Prices should be inserted into the database."
+        self.assertTrue(price_1 is not None and price_2 is not None, "Prices should be inserted into the database.")
+
+        # Test invalid case
+        invalid_case = [
+            (1, '2025-03-03', 10.0, 11.0, 12.0, 9.0, 10.5, 1000, '1d'),
+            (2, '2025-03-03', 11.0, 12.0, 13.0, 10.0, 11.5, 2000, '1d'),
+        ]
+
+        with self.assertRaises(sqlite3.IntegrityError):
+            db.insert_prices(invalid_case)
+
+        # Check if the database is still consistent
+        cursor.execute(f"SELECT * FROM {db.Tables.HISTORY} WHERE date='2025-03-03';")
+        price_3 = cursor.fetchall()
+        self.assertEqual(len(price_3), 0, "No prices should be inserted after an error.")
+        cursor.close()
+
 
     def test_get_security(self):
         """Test getting a security from the database."""
+        # Insert an exchange first
+        db.insert_exchange('Shanghai Stock Exchange', 'SSE', 'stock')
+
         # Insert a security first
         db.insert_security('000001', 'Ping An Bank', 'stock', 1)
 
