98caaeb0264bfa14dfb4eadca28685010fdae9d5
wsc0der <wsc0der@outlook.com>
2025-04-22 03:23:31 +0000

    feat: Add a logger to be used in the module

---
+++ rock/__init__.py
@@ -0,0 +1,19 @@
+"""
+rock
+"""
+
+import logging
+# Configure the logging system
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the minimum log level
+    format='[%(asctime)s][%(name)s][%(levelname)s][%(filename)s] %(message)s',  # Log format
+    handlers=[
+        logging.StreamHandler(),  # Log to console
+        logging.FileHandler('app.log')  # Log to a file
+    ]
+)
+logging.getLogger('urllib3').setLevel(logging.INFO)
+
+logger = logging.getLogger('rock')
+
+__all__ = ['logger']
